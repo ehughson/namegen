6fdb2aa2ad33cc661c54dac1bacca4abe07424f3
Emma <48578911+ehughson@users.noreply.github.com>
2020-10-22 15:33:43 -0600

    Update views.py

--- views.py
+++ views.py
@@ -1,12 +1,13 @@
 #from app import app
 from flask import Flask, render_template, request, redirect, url_for, session
-import numpy as np 
+import numpy as np
+import random
 
 app = Flask(__name__)
 @app.route('/')
 #@app.route('/home', methods=['GET','POST'])
-def index():
-	return render_template('index.html', text = "PRESS FOR NAME")
+#def index():
+#	return render_template('index.html', text = "PRESS FOR NAME")
 
 
 @app.route('/home', methods=['GET','POST'])
@@ -21,53 +22,57 @@ def home():
 			lines = open(fname, 'r').readlines()
 			names = []
 			for line in lines:
-				ls = line.decode('utf-8').strip().split(" ")
+				#ls = line.decode('utf-8').strip().split(" ")
+				ls = line.strip().split(" ")
 				int_table = []
 				name = ls[0]
 				int_table.append(name)
 				if (len(ls) > 1) :
 					rarity = ls[1]
-					if rarity == "RARE" : 
+					if rarity == "RARE" :
 						int_table.append("RARE")
-					else : 
-						int_table.append("common") 
-				else : 
+						#print("updating with rare rarity")
+						descriptions =' '.join(ls[2:])
+					else :
+						int_table.append("common")
+						descriptions =' '.join(ls[1:])
+				else :
 					int_table.append("RARE")
-				descriptions =' '.join(ls[1:])
+				#descriptions =' '.join(ls[1:])
 				#print(descriptions)
 				int_table.append(descriptions)
 				names.append(int_table)
 
 			return names
 
-		sunce_pre_names = filter_names("sunce_pre.txt")
+		sunce_pre_names = filter_names("static/sunce_pre.txt")
 		#print(sunce_pre_names)
 
-		sunce_suf_names = filter_names("sunce_suf.txt")
+		sunce_suf_names = filter_names("static/sunce_suf.txt")
 		#print(sunce_suf_names)
 
-		asra_pre_names = filter_names("asra_pre.txt")
+		asra_pre_names = filter_names("static/asra_pre.txt")
 		#print(asra_pre_names)
 
-		asra_suf_names = filter_names("asra_suf.txt")
+		asra_suf_names = filter_names("static/asra_suf.txt")
 		#print(asra_suf_names)
 
-		cansu_pre_names = filter_names("cansu_pre.txt")
+		cansu_pre_names = filter_names("static/cansu_pre.txt")
 		#print(cansu_pre_names)
 
-		cansu_suf_names = filter_names("cansu_suf.txt")
+		cansu_suf_names = filter_names("static/cansu_suf.txt")
 		#print(asra_suf_names)
 
-		misc_pre_names = filter_names("misc_pre.txt")
+		misc_pre_names = filter_names("static/misc_pre.txt")
 		#print(misc_pre_names)
 
-		misc_suf_names = filter_names("misc_suf.txt")
+		misc_suf_names = filter_names("static/misc_suf.txt")
 		#print(misc_suf_names)
 
-		fail_pre_names = filter_names("fail_pre.txt")
+		fail_pre_names = filter_names("static/fail_pre.txt")
 		#print(fail_pre_names)
 
-		fail_suf_names = filter_names("fail_suf.txt")
+		fail_suf_names = filter_names("static/fail_suf.txt")
 		#print(fail_suf_names)
 
 
@@ -87,21 +92,21 @@ def home():
 		d = {}
 
 
-		add_to_dict(d, sunce_pre_names, "sunce", 'suffix')
-		add_to_dict(d, sunce_suf_names, "sunce", "prefix")
+		add_to_dict(d, sunce_pre_names, "sunce", 'prefix')
+		add_to_dict(d, sunce_suf_names, "sunce", "suffix")
 
-		add_to_dict(d, asra_pre_names, "sunce", 'suffix')
-		add_to_dict(d, asra_suf_names, "sunce", "prefix")
+		add_to_dict(d, asra_pre_names, "sunce", 'prefix')
+		add_to_dict(d, asra_suf_names, "sunce", "suffix")
 
-		add_to_dict(d, cansu_pre_names, "sunce", 'suffix')
-		add_to_dict(d, cansu_suf_names, "sunce", "prefix")
+		add_to_dict(d, cansu_pre_names, "sunce", 'prefix')
+		add_to_dict(d, cansu_suf_names, "sunce", 'suffix')
 
-		add_to_dict(d, misc_pre_names, "sunce", 'suffix')
-		add_to_dict(d, misc_suf_names, "sunce", "prefix")
+		add_to_dict(d, misc_pre_names, "sunce", 'prefix')
+		add_to_dict(d, misc_suf_names, "sunce", "suffix")
 
 
-		add_to_dict(d, fail_pre_names, "sunce", 'suffix')
-		add_to_dict(d, fail_suf_names, "sunce", "prefix")
+		add_to_dict(d, fail_pre_names, "sunce", 'prefix')
+		add_to_dict(d, fail_suf_names, "sunce", "suffix")
 		#print(d['Haze']['description'])
 		# add all the other files to this dictions using this function
 
@@ -120,57 +125,69 @@ def home():
 		pre_names = []
 		pre_rarities = []
 		pre_descriptions = []
+		rare_pre_names = []
 		for name, c in pre_d.items():
 			pre_names.append(name)
 			pre_descriptions.append(c['description'])
 			pre_rarities.append(c['rarity'])
+			if c['rarity'] == "RARE":
+				rare_pre_names.append(name)
 
 
 
-		pre_id = []
+
+		pre_prob = []
 		for r in pre_rarities:
 			if r == "RARE":
-				pre_id.append(-7)
+				pre_prob.append(1)
 			else:
-				pre_id.append(0)
+				pre_prob.append(20)
 
 
 		#pre_id = np.array([-7 for r in pre_rarities if r == "RARE" else 0])
-		pre_prob = np.exp(pre_id) / np.exp(pre_id).sum() #normalize probabilities
-		chosen_pre = np.random.choice(pre_names, p=pre_prob)
+		#pre_prob = np.exp(pre_id) / np.exp(pre_id).sum() #normalize probabilities
+		#chosen_pre = np.random.choice(pre_names, p=pre_prob)
+		chosen_pre = random.choices(pre_names, pre_prob, k = 1)[0]
 		pre_description = d[chosen_pre]['description']
 
 
 		suf_names = []
 		suf_rarities = []
+		rare_suf_names = []
 		for name, c in suf_d.items():
 			if name != chosen_pre:
 				suf_names.append(name)
 				suf_rarities.append(c['rarity'])
+				if c['rarity'] == "RARE":
+					rare_suf_names.append(name)
+
 		#print(len(suf_names))
 		#print(len(suf_prob))
-		suf_id = []
+		suf_prob = []
 		for r in suf_rarities:
 			if r == "RARE":
-				suf_id.append(-7)
+				suf_prob.append(1)
 			else:
-				suf_id.append(0)
+				suf_prob.append(20)
 
-		suf_prob = np.exp(suf_id) / np.exp(suf_id).sum() #normalize probabilities
+		#suf_prob = np.exp(suf_id) / np.exp(suf_id).sum() #normalize probabilities
 		#print(len(suf_names))
 		#print(len(suf_prob))
 
-		chosen_suf = np.random.choice(suf_names, p=suf_prob)
+		#chosen_suf = np.random.choice(suf_names, p=suf_prob)
+		chosen_suf = random.choices(suf_names, suf_prob, k = 1)[0]
 		suf_description = d[chosen_suf]['description']
 
-		print("YOU ARE {} {}".format(chosen_pre, chosen_suf))
-		print("The name " + chosen_pre + " means: " + pre_description)
-		print("The name " + chosen_suf + " means: " + suf_description)
+		#print("YOU ARE {} {}".format(chosen_pre, chosen_suf))
+		#print("The name " + chosen_pre + " means: " + pre_description)
+		#print("The name " + chosen_suf + " means: " + suf_description)
 
-		return render_template('home.html', pre_name = chosen_pre, suf_name = chosen_suf, s_desc = "The name " + chosen_suf + " means: " + suf_description, p_desc = "The name " + chosen_pre + " means: " + pre_description)
+
+
+		return render_template('home.html', pre_name = chosen_pre, suf_name = chosen_suf, s_desc =  chosen_suf + " " + suf_description, p_desc =  chosen_pre + " " + pre_description)
 	else:
 		return render_template('home.html')
 
 if __name__ == '__main__':
 	app.debug = True
-	app.run(host = '0.0.0.0', port = 5000)
+	#app.run(host = '0.0.0.0', port = 5000)
